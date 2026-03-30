dc4278cd3f95108f3524a66afd4fbf0d107b42c7
asteenburg <97249534+asteenburg@users.noreply.github.com>
2026-03-30 05:55:45 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -282,9 +282,10 @@ export default function Home() {
         <div className='relative mt-24 z-30 px-6 lg:pl-20 lg:w-[45%] text-center lg:text-left'>
           <h1 className='text-6xl lg:text-[8rem] xl:text-[10rem] text-white font-black tracking-tighter uppercase leading-[0.8]'>
             Stickers <br /> That Match Your <br />
-            <span className='HoseFont.style.fontFamily text-orange-500 normal-case text-8xl lg:text-[12rem] xl:text-[14rem] inline-block -rotate-3 mt-4'>
-              Vibe
-            </span>
+            <span className={`${HoseFont.className} text-orange-500 normal-case text-8xl lg:text-[12rem] xl:text-[14rem] inline-block -rotate-3 mt-4`}>
+  Vibe
+</span>
+
           </h1>
           <p className='mt-12 text-gray-400 text-lg lg:text-xl font-bold uppercase tracking-[0.3em] max-w-md mx-auto lg:mx-0'>
             Premium High-Heat Decals
